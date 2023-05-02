f6abe10e9b351d6d0d2e9a67fc4510223b8b3c50
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-05-02 19:22:26 -0300

    using for

--- Loop/loop.js
+++ Loop/loop.js
@@ -1,13 +1,25 @@
 
+let contador = 1
+
+while (contador <= 10) {
+    console.log(`Contador = ${contador}`)
+    contador++
+}
+
+for (let i = 1; contador <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+const notas = [9.0, 8.4, 9.9, 7.8, 8.3]
+
+for (let i = 0; i < notas.length; i++){
+    console.log(`Nota = ${notas[i]}`)
+}
+    
 
 
 
-
-
-
-
-
-function getInteiroAleatorio(min, max) {
+/*function getInteiroAleatorio(min, max) {
     const valor = Math.random() * (max - min) + min
     return Math.floor(valor)
     
@@ -22,4 +34,4 @@ let opcao = 0
 
 while (opcao != -1)
 
-console.log('Até a próxima!')
+console.log('Até a próxima!')*/
