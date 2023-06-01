2fdce4eca50f3f6874d9abc1de56fa1b381d1d7f
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-06-01 10:27:29 -0300

    IIFE

---
+++ Funções/IIFE.js
@@ -0,0 +1,8 @@
+// IIFE -> Immediately Invoked Function Express
+
+(function () {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente')
+})()
+
+// Esse tipo de função, quando usada, foge do escopo global, é incocada no momento da criação
