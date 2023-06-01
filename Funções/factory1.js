d937c311a42e36d8983d1a78cb445832c83cb92f
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-06-01 08:09:54 -0300

    factory 1

---
+++ Funções/factory1.js
@@ -0,0 +1,11 @@
+// Factory simples
+
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Carvalho'
+    }
+}
+
+// Função factory, é uma função que retorna um objeto (cria um objeto quando é chamada)
+
