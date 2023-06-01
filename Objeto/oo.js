ec2bbda18315953a1e1184a997e981f3456e66f3
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-06-01 15:15:18 -0300

    OO

---
+++ Objeto/oo.js
@@ -0,0 +1,33 @@
+// Código não executável
+
+//Procedural
+
+processamento(valor1, valor2, valor3)
+
+//OO
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processing() {
+        
+    }
+}
+
+objeto.processing() //Foco passou a ser o objeto
+
+/*
+Princípios importantes
+
+1. Abstração - Pegar um objeto do mundo real e "traduzir" para o sistema
+
+2. Encapsulamento - Interface simples, abstração de informações que não serão necessárias para o usuário
+
+3. Herança - Um objeto filho pode herdar atributos e métidos de um objeto pai
+
+4. Polimorfismo -  capacidade de um objeto poder ser referenciado de maneiras diferentes, assumindo múltiplas formas ou comportamentos, enquanto mantém uma única interface.
+
+
+ */
