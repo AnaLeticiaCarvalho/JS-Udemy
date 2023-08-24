854447805c116fe63735998dc61609028551dd9c
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-08-24 17:46:14 -0300

    mais exemplos de uso do forEach

--- Array/foreach1.js
+++ Array/foreach1.js
@@ -1,10 +1,14 @@
+Array.prototype.forEach2 = function (callback) {
+    for (let i = 0; i < this.length; i++)
+    {
+        callback(this[i], i, this)
+        }
+
+}
+
 const aprovados = ['Agatha', 'Joana', 'Milena', 'Marcus']
 
-aprovados.forEach(function (nome, indice) {
+aprovados.forEach2(function (nome, indice) {
     console.log(`${indice + 1} ${nome}`)
 })
 
-aprovados.forEach(nome => console.log(nome))
-
-const exibirAprovados = aprovado => console.log(aprovado)
-aprovados.forEach(exibirAprovados)
