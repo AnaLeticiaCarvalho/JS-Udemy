bdf4497fa976579c3b2dccec6dd653a5609227ec
Ana Leticia Carvalho <analeticiacarvalho01@gmail.com>
2023-09-22 16:52:58 -0300

    continuando

--- node/moduloCliente.js
+++ node/moduloCliente.js
@@ -1,7 +1,8 @@
 const moduloA = require('./moduloA')
 const moduloB = require('./moduleB')
-
+const lodash = require('lodash')
 //forma de importar com es6
 
-import moduloA from './moduloA'
-import moduloB from './moduleB'
+/*import moduloA from './moduloA'
+import moduloB from './moduleB'*/
+//lodash é uma biblioteca
